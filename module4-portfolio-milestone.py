9d915836c31e0a0cac6b0bbb1c4aa8682f845ef8
Matthew <matthew.bayne@csuglobal.edu>
2023-10-08 18:05:46 -0500

    Matthew - Portfolio Milestone - 2023-10-08

--- module4-portfolio-milestone.py
+++ module4-portfolio-milestone.py
@@ -1,15 +1,8 @@
-#WIP
-#Build the ItemToPurchase class.
+# CSC500 - Portfolio Milestone - Matthew Bayne
+#=============================================
 
+#Build the ItemToPurchase class
 class ItemToPurchase():
-    #Attributes
-    #item_name (string)
-    #item_price (float)
-    #item_quantity (int)
-    #Default constructor
-    #Initializes item's name = "none", item's price = 0, item's quantity = 0
-    #Method
-    #print_item_cost()
     def __init__(self, item_num, item_cost = 0, item_name = 'none', item_price = 0.0, item_quantity = 0):
         self.item_num = item_num
         self.item_name = item_name
@@ -18,9 +11,7 @@ class ItemToPurchase():
         self.item_cost = item_cost
 
     def request_input(self):
-        print(r'//========\\')
-        print( '|| Item', self.item_num, '||')
-        print(r'\\========//')
+        print( 'Item', self.item_num)
         self.item_name = input('Enter the item name:\n')
         self.item_price = float(input('Enter the item price:\n'))
         self.item_quantity = int(input('Enter the item quantity:\n'))
@@ -30,17 +21,14 @@ class ItemToPurchase():
         print('{name} {quantity:.0f} @ ${price:.2f} = ${cost:.2f}'.format(name = self.item_name, quantity = self.item_quantity, price = self.item_price, cost = self.item_cost))
 
 #Prompt the user for two items and create two objects of the ItemToPurchase class.
-
 item1 = ItemToPurchase(item_num = 1)
 item2 = ItemToPurchase(item_num = 2)
+print()
+item1.request_input()
+item2.request_input()
 
 #Add the costs of the two items together and output the total cost.
-
-print()
-print(' TOTAL COST')
-item1.request_input()
+print('TOTAL COST')
 item1.print_item_cost()
-item2.request_input()
 item2.print_item_cost()
-
 print('Total: ${0:.2f}'.format(item1.item_cost + item2.item_cost))
