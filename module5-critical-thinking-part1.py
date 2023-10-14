23f24a4001b1a93b548fe7f6af4da8cfba9d6182
Matthew <matthew.bayne@csuglobal.edu>
2023-10-14 03:25:42 -0500

    Matthew - Module 5 Refinement - 20231014

--- module5-critical-thinking-part1.py
+++ module5-critical-thinking-part1.py
@@ -1,37 +1,25 @@
 # CSC500 - Critical Thinking - Part 1 - Matthew Bayne
 #====================================================
 
-'''
-Input number of years
-Var rainfall data
-
-For year in years
-    For month in 12 months
-        Input monthly rainfall
-
-Output total months
-Output total rainfall
-Output average rainfall
-'''
-
+#Setup Header
 print('Average Rainfall Calculator')
 print('---------------------------')
 
+#Setup Month Dictionary, initialize variable
 month_data = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
-rainfall_data = {}
 total_rainfall = 0
 
+#Take input, calculate months from years.
 total_years = int(input('Please input the number of years: '))
 total_months = total_years * 12
 
-for year in range(0, (total_years), 1):
-    for month in range(1, 13):
-        current_rainfall = int(input('How many inches of rainfall for {}: '.format(month_data[month])))
-        rainfall_data.update({((year * 12) + month):current_rainfall})
-        total_rainfall += current_rainfall
+for year in range(0, (total_years), 1): #Iterate over the number of years
+    for month in range(1, 13): #Iterate over 12 months
+        current_rainfall = float(input('How many inches of rainfall for {}: '.format(month_data[month]))) #Float to account for small rainfall amounts
+        total_rainfall += current_rainfall #Add to total rainfall
 
 average_rainfall = total_rainfall / total_months
 
-print('Total months: {} months'.format(total_months))
+print('\nTotal months: {} months'.format(total_months))
 print('Total inches of rainfall: {:.2f} inches'.format(total_rainfall))
 print('Average rainfall per month: {:.2f} inches'.format(average_rainfall))
--- module5-critical-thinking-part2.py
+++ module5-critical-thinking-part2.py
@@ -1,27 +1,11 @@
 # CSC500 - Critical Thinking - Part 2 - Matthew Bayne
 #====================================================
 
-'''
-Input number of books this month
-Var points equals 0
-
-If book number is >= 8
-    points = 60
-Else If book numer is >= 6 And book number is <= 7
-    points = 30
-Else If book numer is >= 4 And book number is <= 5
-    points = 15
-Else If book numer is >= 2 And book number is <= 3
-    points = 5
-Else
-    ponts = 0
-
-Output points
-'''
-
-print('Welcome to the CSU Global Bookstore Book Club Points Calculator, CGBBCPC!')
+#Setup intro, input var
+print('Welcome to the CSU Global Bookstore Book Club Points Calculator, CGBBCPC!\n')
 books_number = int(input('Please enter the number of books purchased this month: \n'))
 
+#Initialize points and message var, defaulted to least possible.
 reward_points = 0
 additional_message = 'You should buy more books!'
 
@@ -38,4 +22,4 @@ elif (books_number >= 2) and (books_number <= 3):
     reward_points = 5
     additional_message = 'I guess that\'s better than nothing.'
 
-print('You have been awarded {} CGBBCPC points this month! {}'.format(reward_points, additional_message))
+print('\nYou have been awarded {} CGBBCPC points this month! {}'.format(reward_points, additional_message))
