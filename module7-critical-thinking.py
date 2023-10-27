5435607823686f59e0756786ab4d0615cb3d019b
Matthew <matthew.bayne@csuglobal.edu>
2023-10-27 00:08:49 -0500

    Matthew - Fixed some comment spacing - 20231027

--- module7-critical-thinking.py
+++ module7-critical-thinking.py
@@ -1,21 +1,21 @@
 # CSC500 - Critical Thinking - Matthew Bayne
 #====================================================
 
-#Create a dictionary containing course numbers and their respective room numbers.
+# Create a dictionary containing course numbers and their respective room numbers.
 room_number_dict = {'CSC101': 3004, 'CSC102': 4501, 'CSC103': 6755, 'NET110': 1244, 'COM241': 1411}
 
-#Create a dictionary containing course numbers and their respective instructor names.
+# Create a dictionary containing course numbers and their respective instructor names.
 instructors_dict = {'CSC101': 'Haynes', 'CSC102': 'Alvarado', 'CSC103': 'Rich', 'NET110': 'Burke', 'COM241': 'Lee'}
 
-#Create a dictionary containing course numbers and their respective meeting times.
+# Create a dictionary containing course numbers and their respective meeting times.
 meeting_time_dict = {'CSC101': '8:00 a.m.', 'CSC102': '9:00 a.m.', 'CSC103': '10:00 a.m.', 'NET110': '11:00 a.m.', 'COM241': '1:00 p.m.'}
 
-#Merge the dictionaries for easier access
+# Merge the dictionaries for easier access
 merged_dict = room_number_dict
 for key in merged_dict.keys():
     merged_dict[key] = room_number_dict[key], instructors_dict[key], meeting_time_dict[key]
 
-#Handler for printing findings
+# Handler for printing findings
 def printResults(key, name):
     print('\n{} Found!'.format(name))
     print('***************************')
@@ -24,7 +24,7 @@ def printResults(key, name):
     print('Instructor Name: {}'.format(merged_dict[key][1]))
     print('Meeting Time: {}'.format(merged_dict[key][2]))
 
-#Logic handlers for searching through the merged_dict
+# Logic handlers for searching through the merged_dict
 def getCourse():
     course_number = input('--> Enter a Course Number: ').upper()
     for key in merged_dict.keys():
@@ -35,7 +35,7 @@ def getCourse():
 
 def getRoom():
     room_number = input('--> Enter a Room Number: ')
-    try: #Error check for non-number input
+    try: # Error check for non-number input
         room_number = int(room_number)
         for key in merged_dict.keys():
             if room_number == merged_dict[key][0]:
@@ -55,15 +55,15 @@ def getInstructor():
 
 def getMeetingTime():
     meeting_time = input('--> Enter a Course Meeting Time: ').lower()
-    meeting_time_cleansed = ''.join(char for char in meeting_time if char.isalnum()) #Sanitize input (remove symbols and spaces) for maximum compatibility
+    meeting_time_cleansed = ''.join(char for char in meeting_time if char.isalnum()) # Sanitize input (remove symbols and spaces) for maximum compatibility
     for key in merged_dict.keys():
-        temp_cleansed = ''.join(char for char in merged_dict[key][2] if char.isalnum()) #Sanitize for comparison
+        temp_cleansed = ''.join(char for char in merged_dict[key][2] if char.isalnum()) # Sanitize for comparison
         if meeting_time_cleansed == temp_cleansed:
             printResults(key, 'Meeting Time')
             return
     print('\nThere\'s no course with that meeting time, \'{}\'.'.format(meeting_time))  
 
-#UI Handler
+# UI Handler
 def main():
     quit_ui = False
     while quit_ui != True:
@@ -93,4 +93,4 @@ def main():
         else:
             print('Please enter valid input!')
 
-main() #Run program
+main() # Run program
